832213e8869dadd600b10c65e797d70913b9fee3
Abhay <praoabhay@gmail.com>
2024-10-24 22:49:46 +0530

    Updated connections.js to cover the password

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'PRAOABHAY_sql', 
+    password: 'hellofriend', 
     database: 'timetable_db' 
 });
 
